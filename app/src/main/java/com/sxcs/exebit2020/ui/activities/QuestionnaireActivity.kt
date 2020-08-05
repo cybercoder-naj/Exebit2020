1719a6684323623004ae2811cb4d14d9d5192f3e
cybercoder-naj <cybercoder.nishant@gmail.com>
2020-08-05 17:44:26 +0530

    Questionnaire mental questions done

--- app/src/main/java/com/sxcs/exebit2020/ui/activities/QuestionnaireActivity.kt
+++ app/src/main/java/com/sxcs/exebit2020/ui/activities/QuestionnaireActivity.kt
@@ -3,23 +3,29 @@ package com.sxcs.exebit2020.ui.activities
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.sxcs.exebit2020.R
+import com.sxcs.exebit2020.databinding.ActivityQuestionnaireBinding
+import kotlinx.android.synthetic.main.layout_onboarding.*
 
-class QuestionnaireActivity: AppCompatActivity() {
+class QuestionnaireActivity : AppCompatActivity() {
 
-
+    private lateinit var binding: ActivityQuestionnaireBinding
+    private var questionNumber = 1
 
     companion object {
         private const val PARAM_1 = "action"
 
-        fun getIntent(context: Context, action: String) = Intent(context, QuestionnaireActivity::class.java).apply {
-            putExtra(PARAM_1, action)
-        }
+        fun getIntent(context: Context, action: String) =
+            Intent(context, QuestionnaireActivity::class.java).apply {
+                putExtra(PARAM_1, action)
+            }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityQuestionnaireBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val action = intent.getStringExtra(PARAM_1)
 
@@ -34,6 +40,48 @@ class QuestionnaireActivity: AppCompatActivity() {
     }
 
     private fun mentalQuestions() {
+        setQuestions()
+        var disease = "None"
+        with(binding) {
+            btnSubmit.setOnClickListener {
+                if (!radYes.isChecked && !radNo.isChecked) {
+                    Toast.makeText(
+                        this@QuestionnaireActivity,
+                        "You have to select Yes/No to continue",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    if (questionNumber == 1) {
+                        if (radYes.isChecked)
+                            questionNumber = 2
+                        else if (radNo.isChecked)
+                            questionNumber = 3
+                    } else if (questionNumber == 2) {
+                        if (radYes.isChecked)
+                            disease = "PTSD"
+                        else if (radNo.isChecked)
+                            disease = "Scitz"
+                    } else if(questionNumber == 3) {
+                        if (radYes.isChecked)
+                            disease = "Depression"
+                        else if (radNo.isChecked)
+                            disease = "Anxiety"
+                    }
+                    if (disease != "None") {
+                        
+                    }
+                }
+                setQuestions()
+            }
+        }
+    }
 
+    private fun setQuestions() {
+        binding.tvQuestion.text = when (questionNumber) {
+            1 -> "Have you been having hallucinations or delusions?"
+            2 -> "Have you been hallucinating about a real event that you witnessed?"
+            3 -> "Have you been feeling sad?"
+            else -> ""
+        }
     }
 }
